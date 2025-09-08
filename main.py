74649a1ca4beb565eafbcd22dc48a41c7e9c12a8
veronika219 <Dmytrenko.V.S@nmu.one>
2025-09-08 16:07:20 +0300

    Added goodbye function to main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
     print("Hello, world!")
     print_hi("User")
+    print_goodbye("User")
 
 def print_hi(name):
     print(f'Hi, {name}!')
+
+def print_goodbye(name):
+    print(f"Goodbye, {name}! See you soon!")
 
 if __name__ == '__main__':
     main()
